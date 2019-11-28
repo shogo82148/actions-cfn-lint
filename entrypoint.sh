210dcbc2e218604758760801cff686ad170df238
Ichinose Shogo <shogo82148@gmail.com>
2019-11-28 15:49:11 +0900

    try again

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,15 @@
 
 export REVIEWDOG_GITHUB_API_TOKEN=$INPUT_GITHUB_TOKEN
 
-cfn-lint --format parseable "$@" \
-    | reviewdog \
+if [ -n "$INPUT_ARGS" ]; then
+    # shellcheck disable=SC2086
+    cfn-lint --format parseable $INPUT_ARGS > /tmp/out.log
+else
+    cfn-lint --format parseable "$@" > /tmp/out.log
+fi
+
+reviewdog \
     -efm='%f:%l:%c:%*[0-9]:%*[0-9]:%t%n:%m' \
     -name="CloudFormation Linter" \
     -reporter="${INPUT_REPORTER}" \
-    -level="${INPUT_LEVEL}"
+    -level="${INPUT_LEVEL}" < /tmp/out.log
