504aa21e31694d72f3dd9b1d0c31ef01904e9fcb
Ichinose Shogo <shogo82148@gmail.com>
2019-11-28 15:16:22 +0900

    add support for args

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 export REVIEWDOG_GITHUB_API_TOKEN=$INPUT_GITHUB_TOKEN
 
-cfn-lint --format parseable "**/*.yaml" \
+cfn-lint --format parseable "$@" \
     | reviewdog \
     -efm='%f:%l:%c:%*[0-9]:%*[0-9]:%t%n:%m' \
     -name="CloudFormation Linter" \
