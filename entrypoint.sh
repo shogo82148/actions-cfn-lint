05ca7977eb58cbdbbcbabfeaf389736f4e1e8c0d
Ichinose Shogo <shogo82148@gmail.com>
2019-11-28 14:43:40 +0900

    add reporter and level option

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,8 @@
 export REVIEWDOG_GITHUB_API_TOKEN=$INPUT_GITHUB_TOKEN
 
 cfn-lint --format parseable "**/*.yaml" \
-    | reviewdog -efm='%f:%l:%c:%*[0-9]:%*[0-9]:%t%n:%m' -name="CloudFormation Linter" -reporter=github-pr-check
+    | reviewdog \
+    -efm='%f:%l:%c:%*[0-9]:%*[0-9]:%t%n:%m' \
+    -name="CloudFormation Linter" \
+    -reporter="${INPUT_REPORTER}" \
+    -level="${INPUT_LEVEL}"
