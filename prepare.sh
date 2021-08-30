b004143fb05215c3003085a7ec58e4dea85b227b
Ichinose Shogo <shogo82148@gmail.com>
2021-08-30 20:35:20 +0900

    improve prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -18,18 +18,24 @@ rm -rf "$WORKING"
 git clone "$ORIGIN" "$WORKING"
 cd "$WORKING"
 
+: bump version
+echo "$VERSION" > VERSION
+git add VERSION
+git commit -m "bump v$MAJOR.$MINOR.$PATCH"
+git push origin main
+
 : checkout releases branch
 git checkout -b "releases/v$MAJOR" "origin/releases/v$MAJOR" || git checkout -b "releases/v$MAJOR" main
 git merge -X theirs --no-ff -m "Merge branch 'main' into releases/v$MAJOR" main || true
 
 : configure to use the pre-built image
-echo "$VERSION" > VERSION
 git checkout main -- action.yml
 perl -i -pe "s(image:\\s*\"Dockerfile\")(image: \"docker://ghcr.io/shogo82148/actions-cfn-lint:$VERSION\")" action.yml
-git add VERSION action.yml
-git commit -m "bump v$MAJOR.$MINOR.$PATCH"
+git add action.yml
+git commit -m "release v$MAJOR.$MINOR.$PATCH"
 
 : publish to GitHub
+git push origin "releases/v$MAJOR"
 git tag -a "v$MAJOR.$MINOR.$PATCH" -m "release v$MAJOR.$MINOR.$PATCH"
 git push origin "v$MAJOR.$MINOR.$PATCH"
 
